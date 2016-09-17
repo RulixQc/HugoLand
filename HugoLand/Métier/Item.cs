3c22e5007048ae9fb145f760f93f3036c45566cd
yanik lyth <yanik lyth>
2016-09-17 13:38:59 -0400

    kden

--- HugoLand/Métier/Item.cs
+++ HugoLand/Métier/Item.cs
@@ -14,20 +14,36 @@ namespace HugoLand.Métier
     /// </summary>
     public static class Item
     {
-        public static void CréerItem()
+        public static void CréerItem(string Description, ICollection<HugoLand.Accès_aux_données.EffetItem> effet, int id, int imageid,
+            ICollection<HugoLand.Accès_aux_données.InventaireHero> invHe, Accès_aux_données.Monde monde, int mondeid, int niveau, string nom, int poids, int quantite,
+            bool ramasse, int reqForce, int reqDex, int reqEnd, int reqInt, int reqNiv, byte[] rowVersion, int valeur)
         {
-            int x = 0;
-            int y = 0;
+            using (Accès_aux_données.Entities context = new Accès_aux_données.Entities())
+            {
+                var items = context.Set<Accès_aux_données.Item>();
+                items.Add(new Accès_aux_données.Item() { x = 0, y = 0, Description = Description, EffetItems = effet, Id = id, ImageId = imageid
+                , InventaireHeroes = invHe, Monde = monde, MondeId = mondeid, Niveau = niveau, Nom = nom, Poids = poids, Quantite = quantite, Ramasse = ramasse
+                , ReqDexterite = reqDex, ReqEndurance = reqEnd, ReqForce = reqForce, ReqIntelligence = reqInt, ReqNiveau = reqNiv, RowVersion = rowVersion, ValeurArgent = valeur});
+                context.SaveChanges();
+            }
         }
 
-        public static void SupprimerItem()
+        public static void SupprimerItem(Accès_aux_données.Item item)
         {
-
+            using (Accès_aux_données.Entities context = new Accès_aux_données.Entities())
+            {
+                context.Items.Remove(item);
+                context.SaveChanges();
+            }
         }
 
-        public static void ModifierQuantité()
+        public static void ModifierQuantité(Accès_aux_données.Item item, int quantite)
         {
-
+            using (Accès_aux_données.Entities context = new Accès_aux_données.Entities())
+            {
+                item.Quantite = quantite;
+                context.SaveChanges();
+            }
         }
     }
 }
